9e515ef2612611a02eb2edf80686ece5fa8e44bc
Abdul Hafizh <hafizhahm123@gmail.com>
2025-11-03 09:23:04 +0700

    exporting instance

--- src/boss.ts
+++ src/boss.ts
@@ -2,11 +2,11 @@ import PgBoss from "pg-boss";
 
 const DATABASE_URL = process.env.DATABASE_URL as string;
 
-const boss = new PgBoss({
+export const boss = new PgBoss({
   connectionString: DATABASE_URL,
 });
 
-async function startBoss() {
+export async function startBoss() {
   boss.on("error", (error) => console.log({ error })); //setelah buat instance, sebelum start
   await boss.start(); //wajib untuk starting
   console.log("[PGBOSS] Service started");
